10542f66acc98de0d55a64dfdb617deaaf7b9fb8
NoFlue <103296212+NoFlue@users.noreply.github.com>
2023-02-09 22:31:09 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1796 ms, Memory: 297352 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
@@ -0,0 +1,26 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val arr = IntArray(10001) { 0 }
+
+    repeat(br.readLine().toInt()) {
+        arr[br.readLine().toInt()]++
+    }
+
+    br.close()
+
+    for(i in 1 until 10001) {
+        repeat(arr[i]) {
+            bw.write("$i\n")
+        }
+    }
+
+    bw.flush()
+    bw.close()
+}
